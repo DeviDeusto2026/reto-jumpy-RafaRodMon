6a218ea35735f90af10370110db05049e8e416b8
Rafael Rodriguez <rafael.rodriguez@opendeusto.es>
2026-04-28 10:35:33 +0200

    Scripts updated (Still not working fully)
    Simpler code, the fox keeps going through the ground.

---
+++ Assets/Jumpy/Scripts/Camara.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Camera : MonoBehaviour
+{
+
+    public float cameraSpeed = 1f;
+    [SerializeField] private Transform fox;
+    private Camera cam;
+    private float maxReachedHeight;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        maxReachedHeight = transform.position.y;
+    }
+
+    void LateUpdate()
+    {
+        if (fox == null) return;
+
+        maxReachedHeight += cameraSpeed * Time.deltaTime;
+
+        float targetHeight = fox.position.y;
+        if (targetHeight > maxReachedHeight)
+        {
+            maxReachedHeight = targetHeight;
+        }
+
+        transform.position = new Vector3(
+            transform.position.x,
+            maxReachedHeight,
+            transform.position.z
+        );
+
+        Vector3 viewportPos = cam.WorldToViewportPoint(fox.position);
+
+        if (viewportPos.y < 0f)
+        {
+            Debug.Log("¡El zorro ha salido de la vista por abajo!");
+            ReiniciarJuego();
+        }
+    }
+
+    private Vector3 WorldToViewportPoint(Vector3 position)
+    {
+        throw new NotImplementedException();
+    }
+
+    void ReiniciarJuego()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Assets/Jumpy/Scripts/FoxMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class FoxMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is 
+    public int movSpeed = 15;
+    public float fSalto = 8f;
+    private bool salta;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        move();
+        jump();
+    }
+
+    private void move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += Vector3.left * Time.deltaTime * movSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += Vector3.right * Time.deltaTime * movSpeed;
+        }
+    }
+
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && salta)
+        {
+            rb.AddForce(Vector3.up * fSalto, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            salta = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            salta = true;
+        }
+    }
+}
